50a4aaad72fc4134ebe6977c408a238cfc4b5d69
Jonne Itkonen <batalyx@users.noreply.github.com>
2017-04-27 08:47:25 +0300

    MarkingFactory handles logic & drawing between mouseDown and mouseUp.

--- Point/DrawableView.swift
+++ Point/DrawableView.swift
@@ -8,21 +8,88 @@
 
 import Cocoa
 
+
 protocol Marking {
     func draw(_ dirtyRect: NSRect);
 }
 
+protocol MarkingFactory {
+    func startEvent(_ e: NSEvent)
+    func endEvent(_ e: NSEvent)
+    func makeMarking() -> Marking?
+}
+
+
 class DrawRect : Marking {
-    var rect: NSRect
-    init( from start: NSPoint, to end: NSPoint) {
-        rect = NSRect(origin: start,
+    var rect: NSRect?
+    var start: NSPoint?
+    var end: NSPoint?
+
+    init(from start: NSPoint, to end: NSPoint) {
+        let rect = NSRect(origin: start,
                       size: NSSize(width: end.x-start.x,
                                    height: fabs(end.y-start.y)))
-        rect = rect.standardized
+        self.rect = rect //.standardized
+//        self.start = nil
+//        self.end = nil
+    }
+
+//    init(from start: NSPoint) {
+//        self.start = start
+//        self.end = nil
+//        self.rect = nil
+//    }
+
+    init(withRect r: NSRect) {
+        self.rect = r
     }
 
     func draw(_ dirtyRect: NSRect) {
-        NSFrameRectWithWidth(rect, 2.0)
+        if let rect = self.rect {
+            NSFrameRectWithWidth(rect, 2.0)
+        }
+    }
+
+    class Factory : MarkingFactory {
+        var start: NSPoint?
+        var end: NSPoint?
+
+        func withStart(_ s: NSPoint) {
+            start = s
+            end = nil
+            NSLog("withstart")
+        }
+
+        func withEnd(_ e: NSPoint) {
+            end = e
+            let sx = min(start!.x, end!.x)
+            let ex = max(start!.x, end!.x)
+            let sy = min(start!.y, end!.y)
+            let ey = max(start!.y, end!.y)
+            start = NSPoint(x: sx, y: sy)
+            end   = NSPoint(x: ex, y: ey)
+            NSLog("withEnd")
+        }
+
+        func startEvent(_ e:NSEvent) {
+            start = e.locationInWindow
+            end = nil
+        }
+
+        func endEvent(_ e:NSEvent) {
+            end = e.locationInWindow
+            let sx = min(start!.x, end!.x)
+            let ex = max(start!.x, end!.x)
+            let sy = min(start!.y, end!.y)
+            let ey = max(start!.y, end!.y)
+            start = NSPoint(x: sx, y: sy)
+            end   = NSPoint(x: ex, y: ey)
+        }
+
+        func makeMarking() -> Marking? {
+            guard start != nil && end != nil else { return nil }
+            return DrawRect(from: start!, to: end!)
+        }
     }
 }
 
@@ -33,6 +100,11 @@ class DrawableView: NSView {
     }
 
     var markings = Array<Marking>()
+
+    var markingStyles = [
+        "r": DrawRect.Factory()
+    ]
+    var currentStyle: MarkingFactory?
 
     override var acceptsFirstResponder: Bool {
         return true
@@ -45,17 +117,7 @@ class DrawableView: NSView {
         ov.stroke()
 
         if start != nil && end != nil {
-            var sp = start!.locationInWindow
-            var ep = end!.locationInWindow
-            let sx = min(sp.x, ep.x)
-            let ex = max(sp.x, ep.x)
-            let sy = min(sp.y, ep.y)
-            let ey = max(sp.y, ep.y)
-            sp = NSPoint(x: sx, y: sy)
-            ep = NSPoint(x: ex, y: ey)
-
-            markings.append(DrawRect(from: sp, to: ep))
-
+            NSLog("why")
             start = nil
             end = nil
         }
@@ -66,13 +128,20 @@ class DrawableView: NSView {
     }
 
     override func mouseDown(with event: NSEvent) {
+        NSLog("mds -")
         guard start == nil else { return }
         start = event
+        currentStyle!.startEvent(event)
+        NSLog("md")
     }
 
     override func mouseUp(with event: NSEvent) {
         guard end == nil else { return }
         end = event
+        currentStyle!.endEvent(event)
+        if let marking = currentStyle?.makeMarking() {
+            markings.append(marking)
+        }
     }
 
     override func mouseMoved(with event: NSEvent) {
@@ -80,15 +149,19 @@ class DrawableView: NSView {
     }
 
     override func keyDown(with event: NSEvent) {
+        if currentStyle == nil {
+            NSLog("curStyle<-")
+            currentStyle = markingStyles["r"]
+        }
         if let keys = event.characters {
-            if keys[keys.startIndex] == "-" {
-                if paths.count > 0 {
-                    paths.removeLast()
-                }
+            if keys == "-" {
                 if markings.count > 0 {
                     markings.removeLast()
                 }
                 self.needsDisplay = true
+         //   } else if pressed in markingStyles {
+            } else if markingStyles.keys.contains(keys) {
+                currentStyle = markingStyles[keys]
             } else {
                 super.keyUp(with: event)
             }
